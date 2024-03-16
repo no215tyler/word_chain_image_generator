595227bb606cce05c11c7c3e1cb2526bb8f99c75
no215tyler <no.215.tyler@gmail.com>
2024-03-16 17:10:27 +0900

    createアクション（プロンプト設定）作成

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,16 @@
 class GamesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
   def index
+  end
+
+  def create
+    words = params[:words].join(", ")
+    translated_words = TranslationService.translate(words)
+    puts translated_words
+    translated_words.prepend("master piece, best quality, ")
+    negative_prompt = "EasyNegative, (worst quality, low quality:1.4), lowres, ugly, bad anatomy, nsfw((Not safe for work)), low quality, negative hand-neg, bad anatomy ,extra fingers, fewer fingers, missing fingers ,extra arms, fewer arms, missing arms, extra legs, fewer legs, extra legs ,text ,logo, watermark, text, word, monochrome, rainbow, wood"
+    image_bytes = StableDiffusionService.query(translated_words, negative_prompt)
+    image_data = Base64.encode64(image_bytes)
+    render json: { image: image_data }
   end
 end
