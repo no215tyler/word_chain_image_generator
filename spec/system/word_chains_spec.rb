0381565dfb1fe3a87f316d58b9b4a074b678a6e4
no215tyler <no.215.tyler@gmail.com>
2024-04-07 22:23:56 +0900

    統合テスト（しりとりルール）

---
+++ spec/system/word_chains_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe "しりとりルール", type: :system do
+  context 'しりとりが続けられる場合' do
+    it '単語が拗音で終わる場合は次の単語の頭文字も拗音で開始する' do
+      visit root_path
+      fill_in 'typing-input', with: 'かいしゃ'
+      click_on('return')
+      fill_in 'typing-input', with: 'あ'
+      click_on('return')
+      expect(page).to have_content('「しゃ」')
+      sleep 2
+      fill_in 'typing-input', with: 'しゃかい'
+      click_on('return')
+      expect(page).to have_no_content('「しゃ」')
+    end
+
+    it '単語が長音記号で終わる場合は次の単語の頭文字は長音記号の前の文字列で開始する' do
+      visit root_path
+      fill_in 'typing-input', with: 'ヒーロー'
+      click_on('return')
+      fill_in 'typing-input', with: 'あ'
+      click_on('return')
+      expect(page).to have_content('「ろ」')
+      sleep 2
+      fill_in 'typing-input', with: 'ろうそく'
+      click_on('return')
+      expect(page).to have_no_content('「ろ」')
+    end
+
+    it '単語が拗音＋長音で終わる場合は次の単語の頭文字は拗音で開始する' do
+      visit root_path
+      fill_in 'typing-input', with: 'イルカショー'
+      click_on('return')
+      fill_in 'typing-input', with: 'あ'
+      click_on('return')
+      expect(page).to have_content('「しょ」')
+      sleep 2
+      fill_in 'typing-input', with: 'ショータイム'
+      click_on('return')
+      expect(page).to have_no_content('「しょ」')
+    end
+  end
+
+  context 'しりとりが続けられない場合' do
+    it '単語が「ん」で終わるとゲームオーバーになる' do
+      visit root_path
+      fill_in 'typing-input', with: 'あかちゃん'
+      click_on('return')
+      expect(page).to have_content('ゲーム終了')
+    end
+  end
+end
