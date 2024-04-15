0f97a6b46d19bd65650781cb29b1cdb301503b61
no215tyler <no.215.tyler@gmail.com>
2024-04-15 10:56:09 +0900

    レスポンシブUXの改善（フリックスライドの動きをスムーズにし、横スクロールを防ぐ）

--- app/javascript/images_index.js
+++ app/javascript/images_index.js
@@ -5,11 +5,13 @@ document.addEventListener('turbo:load', () => {
   let autoSlideInterval = null;
   // タッチイベントに関する設定
   let startX;
+  let startY;
   const slider = document.querySelector('.slider');
 
   // タッチ開始時のハンドラー
   slider.addEventListener('touchstart', function(e) {
     startX = e.touches[0].pageX;
+    startY = e.touches[0].pageY;
   });
 
   // タッチ終了時のハンドラー
@@ -21,14 +23,32 @@ document.addEventListener('turbo:load', () => {
       moveSlides(-1);
     }
     restartAutoSlide();
-  });  
+  });
 
-  document.querySelector('.prev').addEventListener('click', () => {
+  slider.addEventListener('touchmove', function(e) {
+    const moveX = e.touches[0].pageX;
+    const moveY = e.touches[0].pageY;
+    const diffX = startX - moveX;
+
+    // Y方向の動きがX方向の動きより小さい場合、横スクロールと判断
+    if (Math.abs(diffX) > Math.abs(startY - moveY)) {
+      e.preventDefault(); // 横スクロールを防ぐ
+    }
+    
+    if (Math.abs(diffX) > 30) {
+      const offset = currentSlide * -450 - diffX;
+      document.querySelector('.slides').style.transform = `translateX(${offset}px)`;
+    }
+  });
+
+  document.querySelector('.prev').addEventListener('click', (e) => {
+    e.stopPropagation(); // イベントの伝播を止める
     moveSlides(-1);
     restartAutoSlide();
   });
 
-  document.querySelector('.next').addEventListener('click', () => {
+  document.querySelector('.next').addEventListener('click', (e) => {
+    e.stopPropagation(); // イベントの伝播を止める
     moveSlides(1);
     restartAutoSlide();
   });
