1c3ea5868ab3e85877f20fd83bae298f6eb374c1
no215tyler <no.215.tyler@gmail.com>
2024-04-16 17:27:46 +0900

    レスポンシブUXの改善（画像スライドボタンが無効化される問題の対処）

--- app/javascript/images_index.js
+++ app/javascript/images_index.js
@@ -14,42 +14,51 @@ document.addEventListener('turbo:load', () => {
     startX = e.touches[0].pageX;
     startY = e.touches[0].pageY;
     isNearEdge = startX < touchBorderWidth || startX > window.innerWidth - touchBorderWidth;
-    if (!isNearEdge) {
-      e.preventDefault();
-    }
-  });
+  }, { passive: false });
 
   // タッチ終了時のハンドラー
   slider.addEventListener('touchend', function(e) {
-    const endX = e.changedTouches[0].pageX;
-    if (startX - endX > 30) { // 右から左へのフリック
-      moveSlides(1);
-    } else if (startX - endX < -30) { // 左から右へのフリック
-      moveSlides(-1);
+    isNearEdge = startX < touchBorderWidth || startX > window.innerWidth - touchBorderWidth;
+    if (!isNearEdge) {
+      e.preventDefault();
+      const endX = e.changedTouches[0].pageX;
+      if (startX - endX > 30) { // 右から左へのフリック
+        moveSlides(1);
+      } else if (startX - endX < -30) { // 左から右へのフリック
+        moveSlides(-1);
+      }
+      restartAutoSlide();
     }
+  }, { passive: false });
+
+  slider.addEventListener('touchmove', function(e) {
+    isNearEdge = startX < touchBorderWidth || startX > window.innerWidth - touchBorderWidth;
+    if (!isNearEdge) {
+      const moveX = e.touches[0].pageX;
+      const moveY = e.touches[0].pageY;
+      const diffX = startX - moveX;
+
+      // Y方向の動きがX方向の動きより小さい場合、横スクロールと判断
+      if (Math.abs(diffX) > Math.abs(startY - moveY)) {
+        e.preventDefault(); // 横スクロールを防ぐ
+      }
+      
+      if (Math.abs(diffX) > 30) {
+        const offset = currentSlide * -450 - diffX;
+        document.querySelector('.slides').style.transform = `translateX(${offset}px)`;
+      }
+    }
+  }, { passive: false });
+
+  document.querySelector('.prev').addEventListener('touchend', (e) => {
+    e.stopPropagation(); // イベントの伝播を止める
     restartAutoSlide();
   });
 
-  slider.addEventListener('touchmove', function(e) {
-    if (isNearEdge) {
-      return
-    }
-    const moveX = e.touches[0].pageX;
-    const moveY = e.touches[0].pageY;
-    const diffX = startX - moveX;
-
-
-    // Y方向の動きがX方向の動きより小さい場合、横スクロールと判断
-    if (Math.abs(diffX) > Math.abs(startY - moveY)) {
-      e.preventDefault(); // 横スクロールを防ぐ
-    }
-    
-    if (Math.abs(diffX) > 30) {
-      const offset = currentSlide * -450 - diffX;
-      document.querySelector('.slides').style.transform = `translateX(${offset}px)`;
-    }
+  document.querySelector('.next').addEventListener('touchend', (e) => {
+    e.stopPropagation(); // イベントの伝播を止める
+    restartAutoSlide();
   });
-
   document.querySelector('.prev').addEventListener('click', (e) => {
     e.stopPropagation(); // イベントの伝播を止める
     moveSlides(-1);
