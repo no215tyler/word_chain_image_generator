12c47125d8abf4f69d023bbd1c5bb5b2cdac5ad1
no215tyler <no.215.tyler@gmail.com>
2024-04-29 21:54:39 +0900

    画像詳細ページの表示アニメーション追加

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        "rotate-in-ver": "rotate-in-ver 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both"
+      },
+      keyframes: {
+          "rotate-in-ver": {
+              "0%": {
+                  transform: "rotateY(-360deg)",
+                  opacity: "0"
+              },
+              to: {
+                  transform: "rotateY(0deg)",
+                  opacity: "1"
+              },
+          },
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
