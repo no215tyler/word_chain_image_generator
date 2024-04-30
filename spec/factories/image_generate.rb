5f172af895e21d36f43201d8fbb5528baba74c0b
no215tyler <no.215.tyler@gmail.com>
2024-04-30 18:12:57 +0900

    テストコード用のモック画像を設定

--- spec/factories/image_generate.rb
+++ spec/factories/image_generate.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
     prompt {'squirrel, water melon, flog, ruby, beer'}
     http_status { 200 }
     association :user
+
+    after(:build) do |image_generate|
+      image_generate.image.attach(io: File.open('public/images/test_image.jpg'), filename:'test_image.jpg')
+    end
   end
 end
