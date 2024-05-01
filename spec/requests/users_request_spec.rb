3ba89781e0e2689b80c308e634e86988864989b1
no215tyler <no.215.tyler@gmail.com>
2024-05-01 12:13:56 +0900

    usersコントローラー単体テスト

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  before do 
+    @user = FactoryBot.create(:user)
+    sign_in @user
+  end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/users/show"
-      expect(response).to have_http_status(:success)
+  describe "GET #show" do
+    it "showアクションにリクエストを送ると正常にレスポンスが返ってくる" do
+      get user_path(@user)
+      expect(response.status).to eq(200)
+    end
+    it "showアクションにリクエストを送るとレスポンスにユーザー名が含まれる" do
+      get user_path(@user)
+      expect(response.body).to include(@user.name)
     end
   end
 
